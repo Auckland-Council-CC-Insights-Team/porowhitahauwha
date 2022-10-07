000ec94afa050d709ffee3a454c7a2a1067dec14
Lee Durbin <lee.durbin@aucklandcouncil.govt.nz>
2022-10-07 14:52:12 +1300

    Improved Roxygen comments for the first two functions

--- R/read.R
+++ R/read.R
@@ -1,33 +1,42 @@
 #' Create a connection to the database
 #'
-#' Creates a connection to the database that can be called later if this function
-#' call is stored in an object.
+#' Creates a read-only connection to the DuckDB database that can be called later if
+#' this function call is stored in an object.
 #'
-#' @return Returns an S4 object. This object is used to communicate with the database engine.
+#' @param db_file the full path to the .duckdb file. By default assumes this is stored
+#' in the Insights Team's SharePoint, sync'd to your local machine.
+#' @param read_only choose whether the connection is read-only or not. Read-only by default.
+#'
+#' @return Returns an S4 object. This object is used to communicate with the
+#' database engine.
 #' @export
-connect_to_database <- function() {
-  DBI::dbConnect(
+#'
+#' @examples
+#' connect_to_database(db_file = ":memory:")
+connect_to_database <- function(db_file = get_db_file_path(), read_only = TRUE) {
+  con <- DBI::dbConnect(
     duckdb::duckdb(),
-    paste0(get_file_storage_path(), "/porowhita_hauwha.duckdb")
+    dbdir = db_file,
+    read_only = read_only
     )
+
+  return(con)
 }
 
-#' Retrieve a list of all facility names
+#' Retrieve the full path to the Porowhita Hauwhā .duckdb file
 #'
-#' Retrieve a list of facility names, variants of the names, and an ID and
-#' facility type classification (asset, space, or entity) for retrieving attributes.
+#' Reads your .Renviron file to get the path to the team's SharePoint Document Library,
+#' as sync'd to your local machine. Appends to this the name of the .duckdb file,
+#' thus returning the full path to the database file to use in the connect_to_database()
+#' function.
 #'
-#' @return A tibble with four columns.
-#' @export
-get_facilities_names <- function() {
-  con <- connect_to_database()
+#' @return A character string containing a file path
+#'
+get_db_file_path <- function() {
+  db_file <- paste0(
+    get_file_storage_path(),
+    "/porowhita_hauwha.duckdb"
+    )
 
-  names <- tbl(con, "names") |>
-    left_join(tbl(con, "facilities_attributes"), by = c("facilities_attributes_id" = "id")) |>
-    select(value, role, facilities_attributes_id, facility_type) |>
-    collect()
-
-  DBI::dbDisconnect(con, shutdown=TRUE)
-
-  return(names)
+  return(db_file)
 }
