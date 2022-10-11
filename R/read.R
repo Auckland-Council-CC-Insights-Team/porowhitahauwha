ac3ba5922f7f6a895aaea12e3a01e33e5f02fc85
Lee Durbin <lee.durbin@aucklandcouncil.govt.nz>
2022-10-11 14:55:39 +1300

    Create functions for connecting either to a live or a test database. Test database is useful for unit tests and Roxygen examples, plus for users to explore the structure of the data.

--- R/read.R
+++ R/read.R
@@ -1,52 +1,102 @@
-#' Create a connection to the database
+#' Create a connection to Porowhita Hauwhā.
 #'
-#' Creates a read-only connection to a DuckDB database that can be used later if
-#' this function call is stored in an object.
+#' Creates a connection to either the live version or the test version
+#' of the Porowhita Hauwhā database. This can be used later if this function call
+#' is stored in an object. The connection to the live database is read-only.
 #'
-#' @param db_file The name of the .duckdb file. By default assumes this is called
-#' "Porowhita Hauwhā".
-#' @param read_only Declares if the connection to the database is read-only.
-#' If \code{read_only = TRUE} (the default), the connection is read-only and multiple
-#' R processes can access the same database file at the same time.
+#' @param test_db Is this a connection to the test database? Defaults to FALSE.
 #'
 #' @return An S4 object. This object is used to communicate with the
-#' database engine.
+#' database engine, either live or test.
 #' @export
 #'
 #' @examples
-#' connect_to_database(db_file = ":memory:")
-connect_to_database <- function(db_file = "porowhita_hauwha.duckdb", read_only = TRUE) {
-  if(db_file == ":memory:") {
-    dbdir = db_file
+#' test_conn <- connect_to_database(test_db = TRUE)
+#' tbl(test_conn, "assets")
+connect_to_database <- function(test_db = FALSE) {
+  if(test_db == TRUE) {
+    conn <- connect_to_test_database()
   } else {
-    dbdir <- get_file_path(db_file)
+    conn <- connect_to_production_database()
   }
 
-  con <- DBI::dbConnect(
+  return(conn)
+}
+
+#' Create a connection to Porowhita Hauwhā.
+#'
+#' Creates a read-only connection to the live version of the Porowhita Hauwhā
+#' database.
+#'
+#' @return An S4 object. This object is used to communicate with the
+#' database engine.
+connect_to_production_database <- function() {
+  dbdir <- get_file_path("porowhita_hauwha.duckdb")
+
+  conn <- DBI::dbConnect(
     duckdb::duckdb(),
     dbdir = dbdir,
-    read_only = read_only
+    read_only = TRUE
     )
 
-  return(con)
+  return(conn)
+}
+
+#' Create a connection to a test version of Porowhita Hauwhā
+#'
+#' @return An S4 object. This object is used to communicate with the
+#' database engine.
+connect_to_test_database <- function() {
+  test_conn <- DBI::dbConnect(
+    duckdb::duckdb(),
+    dbdir = ":memory:",
+  ) |>
+    create_test_database()
+
+  return(test_conn)
+}
+
+#' Create a database with test data
+#'
+#' Replicates the structure of Porowhita Hauwhā but fills it with a small sample
+#' of test data.
+#'
+#' @param test_conn The database instance used to connect to the in-memory database.
+#'
+#' @return An S4 object. This object is used to communicate with the
+#' test database engine.
+create_test_database <- function(test_conn) {
+ DBI::dbWriteTable(test_conn, "assets", test_assets, overwrite = TRUE)
+ DBI::dbWriteTable(test_conn, "spaces", test_spaces, overwrite = TRUE)
+ DBI::dbWriteTable(test_conn, "entities", test_entities, overwrite = TRUE)
+ DBI::dbWriteTable(test_conn, "facilities_attributes", test_facilities_attributes, overwrite = TRUE)
+ DBI::dbWriteTable(test_conn, "entity_bridge_table", test_entity_bridge_table, overwrite = TRUE)
+ DBI::dbWriteTable(test_conn, "names", test_names, overwrite = TRUE)
+
+ return(test_conn)
 }
 
 #' Disconnect from the database
 #'
-#' Explicitly shuts down the database instance associated with the connection, \code{con}.
+#' Explicitly shuts down the database instance associated with the connection, \code{conn}.
 #'
-#' @param con The database instance that was called using \code{connect_to_database()}
+#' @param conn The database instance that was called using \code{connect_to_database()}
+#' @param test Are you attempting to disconnect from the test database? Defaults to FALSE.
+#' @param confirm Should the function print a confirmation message to the console? Defaults to TRUE.
 #'
-#' @return A confirmation statement printed to the console.
+#' @return An optional confirmation statement printed to the console.
 #' @export
 #'
 #' @examples
 #' con <- connect_to_database(":memory:")
 #' disconnect_from_database(con)
-disconnect_from_database <- function(con) {
-  DBI::dbDisconnect(con, shutdown=TRUE)
+disconnect_from_database <- function(conn, test = FALSE, confirm = TRUE) {
+  DBI::dbDisconnect(conn, shutdown=TRUE)
 
-  print("Disconnected from the database.")
+  if(confirm == TRUE) {
+    db_type <- dplyr::if_else(test == TRUE, "test", "live")
+    print(paste0("Disconnected from the ", db_type, " database."))
+  }
 }
 
 #' Retrieve a list of all assets
@@ -56,19 +106,20 @@ disconnect_from_database <- function(con) {
 #'
 #' @param ... Optional expressions to filter the asset list, defied in terms of the
 #' variables in the returned tibble.
-#' @param con The database instance. Defaults to what is returned from calling
-#' \code{connect_to_database()}.
+#' @param test_db Retrieve this data from the test database? Defaults to FALSE.
 #'
 #' @return A tibble with six variables.
 #' @export
 #'
 #' @examples
-#' # Retrieve a list of assets that are rural halls in the Rodney Local Board (code not run)
-#' # get_assets(designation == "Rural Hall" & local_board == "Rodney")
-get_assets <- function(..., con = connect_to_database()) {
-  assets <- tbl(con, "assets") |>
+#' # Retrieve a list of assets that are classed as rural halls from the test database
+#' get_assets(designation == "Rural Hall", test = TRUE)
+get_assets <- function(..., test = FALSE) {
+  conn <- connect_to_database(test_db = test)
+
+  assets <- tbl(conn, "assets") |>
     left_join(
-      tbl(con, "facilities_attributes"),
+      tbl(conn, "facilities_attributes"),
       by = c("id" = "facility_id"),
       suffix = c(".assets", "facilities_attributes")
       ) |>
@@ -76,9 +127,10 @@ get_assets <- function(..., con = connect_to_database()) {
     select(facility_id = .data$id.assets, .data$name, .data$local_board, .data$designation, .data$delivery_model) |>
     collect()
 
+  disconnect_from_database(conn, test = test, confirm = FALSE)
+
   return(assets)
 }
-
 
 #' Retrieve the full path to a file in File Storage.
 #'
