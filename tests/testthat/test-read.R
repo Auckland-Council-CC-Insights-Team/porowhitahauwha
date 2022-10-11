a098f05689cd83d3ee77f39546efb38bbdd62ae8
Lee Durbin <lee.durbin@aucklandcouncil.govt.nz>
2022-10-11 16:12:34 +1300

    Updated the tests after doing further development of the functions.

--- tests/testthat/test-read.R
+++ tests/testthat/test-read.R
@@ -1,28 +1,12 @@
-test_that("it's possible to connect to DuckDB in-memory", {
-  con <- connect_to_database(db_file = ":memory:")
-  expect_s4_class(con, "DBIConnection")
-  DBI::dbDisconnect(con, shutdown=TRUE)
+test_that("it's possible to connect to the test database", {
+  test_conn <- connect_to_database(test_db = TRUE)
+  expect_s4_class(test_conn, "DBIConnection")
+  disconnect_from_database(test_conn, test = TRUE, confirm = FALSE)
 })
 
-Sys.unsetenv("MY_SHAREPOINT_FILES")
-readRenviron(here(".Renviron"))
-if(identical(sub('.*(?=.{7}$)', '', Sys.getenv("MY_SHAREPOINT_FILES"), perl = T), "Storage")) {
-  test_that("we can construct a file path to the .duckdb file", {
-    expect_equal(
-      sub('.*(?=.{84}$)', '', get_file_path("porowhita_hauwha.duckdb"), perl=T),
-      "\\Auckland Council\\CC Insights & Analysis Team - File Storage\\porowhita_hauwha.duckdb"
-    )
-  })
-}
-Sys.unsetenv("MY_SHAREPOINT_FILES")
-
-test_that("we can retrieve a list of assets", {
-  con <- connect_to_database(":memory:")
-  DBI::dbWriteTable(con, "assets", assets)
-  DBI::dbWriteTable(con, "facilities_attributes", facilities_attributes)
-  asset_sample <- c("Albany House", "Acacia Court Hall", "Buckland Community Centre")
-  all_assets <- get_assets(con = con) |> dplyr::pull(name)
-  DBI::dbDisconnect(con, shutdown=TRUE)
+test_that("we can retrieve a list of assets from the test database", {
+  asset_sample <- c("Buckland Community Centre", "Franklin The Centre")
+  all_assets <- get_assets(test = TRUE) |> dplyr::pull(name)
 
   expect_true(
     setdiff(asset_sample, all_assets) |>
