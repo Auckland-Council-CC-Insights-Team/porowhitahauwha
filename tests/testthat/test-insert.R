bd4fa6f8a620df0da87ce24087691ef80851d666
Lee Durbin <lee.durbin@aucklandcouncil.govt.nz>
2022-10-12 20:48:50 +1300

    Unit tests to check get_new_id() works as expected.

--- tests/testthat/test-insert.R
+++ tests/testthat/test-insert.R
@@ -3,3 +3,21 @@ test_that("it's possible to connect to the test database", {
   expect_s4_class(test_conn, "DBIConnection")
   disconnect_from_database(test_conn, test = TRUE, confirm = FALSE)
 })
+
+test_that("we can create a new ID for the assets table", {
+  test_conn <- connect_to_writable_database(test_db = TRUE)
+  current_ids <- tbl(test_conn, "assets") |> pull(id)
+  new_id_num <- get_new_id(test_conn, "assets")
+  new_id <- paste0("A", new_id_num)
+  expect_false(new_id %in% current_ids)
+  disconnect_from_database(test_conn, test = TRUE, confirm = FALSE)
+})
+
+test_that("we can create a new ID for the facilities_attributes table", {
+  test_conn <- connect_to_writable_database(test_db = TRUE)
+  current_ids <- tbl(test_conn, "facilities_attributes") |> pull(id)
+  new_id_num <- get_new_id(test_conn, "facilities_attributes")
+  new_id <- paste0("FA", new_id_num)
+  expect_false(new_id %in% current_ids)
+  disconnect_from_database(test_conn, test = TRUE, confirm = FALSE)
+})
