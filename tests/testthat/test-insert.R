08f4220d9c05dda02c715a45fac73980b76cb402
Lee Durbin <lee.durbin@aucklandcouncil.govt.nz>
2022-10-14 09:28:23 +1300

    Removed connect and disconnect functions as this is handled in the function itself

--- tests/testthat/test-insert.R
+++ tests/testthat/test-insert.R
@@ -3,7 +3,9 @@ test_that("we can create a new ID for the assets table", {
   current_ids <- tbl(test_conn, "assets") |> pull(id)
   new_id_num <- get_new_id(test_conn, "assets")
   new_id <- paste0("A", new_id_num)
+
   expect_false(new_id %in% current_ids)
+
   disconnect_from_database(test_conn, test = TRUE, confirm = FALSE)
 })
 
@@ -12,17 +14,18 @@ test_that("we can create a new ID for the facilities_attributes table", {
   current_ids <- tbl(test_conn, "facilities_attributes") |> pull(id)
   new_id_num <- get_new_id(test_conn, "facilities_attributes")
   new_id <- paste0("FA", new_id_num)
+
   expect_false(new_id %in% current_ids)
+
   disconnect_from_database(test_conn, test = TRUE, confirm = FALSE)
 })
 
 test_that("we can add a new partner to the test database", {
-  test_conn <- connect_to_writable_database(test_db = TRUE)
   new_partner <- insert_partner(
     name = "Hobbiton Charitable Trust",
     facility_owner = FALSE,
     test_db = TRUE
     )
+
   expect_equal(new_partner |> pull(name), "Hobbiton Charitable Trust")
-  disconnect_from_database(test_conn, test = TRUE, confirm = FALSE)
 })
