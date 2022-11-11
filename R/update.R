455b52823e4fa0842528f8c9e37fed92941744c3
Lee Durbin <lee.durbin@aucklandcouncil.govt.nz>
2022-11-11 19:58:58 +1300

    Created functions and associated tests for updated information about facilities. A full day's work!

--- R/update.R
+++ R/update.R
@@ -1,62 +1,156 @@
-update_facility <- function(facility_name = NULL, field = c(...), value = c(...), test_db = FALSE) {
+name_update_check <- function(field) {
+  if(field == "name") {
+    rlang::abort(
+      "You cannot update the facility's primary name this way. Please use insert_name() instead."
+    )
+  }
 
+  return(TRUE)
+}
+
+record_count_check <- function(facility_to_update) {
+  if(nrow(facility_to_update) != 1) {
+    rlang::abort(
+      "You must update exactly one facility when calling this function."
+    )
+  }
+
+  return(TRUE)
+}
+
+update_address <- function(facility_name, value, test_db = FALSE) {
+  updated_address <- update_asset(
+    facility_name,
+    field = "physical_address",
+    value = value,
+    test_db = test_db
+  )
+
+  return(updated_address)
+
+}
+
+update_asset <- function(facility_name, field, value, test_db = FALSE) {
+  asset_id <- get_assets(test_db = test_db) |>
+    filter(name == facility_name) |>
+    pull(facility_id)
+  updates <- paste0(field, "='", value, "'")
+  record_to_update = paste0("id='", asset_id, "'")
+
+  updated_asset <- update_record(updates, record_to_update, test_db = test_db, tbl_name = "assets")
+
+  return(updated_asset)
+}
+
+update_facilities_attributes <- function(facility_name, field, value, test_db = FALSE) {
+  facility_id <- verify_facility_update(facility_name, field, value, test_db) |>
+    pull(facility_id)
+
+  updated_attributes <- purrr::map2_dfr(
+    purrr::map2_chr(field, value, ~paste0(.x, "='", .y, "'")),
+    paste0("facility_id = '", facility_id, "'"),
+    ~update_record(updates = .x, record_to_update = .y, test_db = test_db, tbl_name = "facilities_attributes")
+  )
+
+  return(updated_attributes)
+}
+
+update_facility <- function(facility_name = NULL, field = list(...), value = list(...), test_db = FALSE) {
+  new_local_board <- find_field("local_board", field)
+  if(new_local_board != FALSE) {
+    update_local_board(facility_name, value[[new_local_board]], test_db = test_db)
+    field <- field[-new_local_board]
+    value <- value[-new_local_board]
+  }
+
+  new_address <- find_field("physical_address", field)
+  if(new_address != FALSE) {
+    update_address(facility_name, value[[new_address]], test_db = test_db)
+    field <- field[-new_address]
+    value <- value[-new_address]
+  }
+
+  if(length(field) > 0) {
+    update_facilities_attributes(facility_name, field, value, test_db = test_db)
+  }
+
+  updated_facility <- get_facilities(name == facility_name, test_db = test_db)
+
+  return(updated_facility)
+}
+
+update_local_board <- function(facility_name, value, test_db = FALSE) {
+  updated_local_board <- update_asset(
+    facility_name,
+    field = "local_board",
+    value = value,
+    test_db = test_db
+  )
+
+  return(updated_local_board)
+
+}
+
+update_record <- function(updates, record_to_update, test_db = FALSE, tbl_name = NULL) {
+  conn <- connect_to_writable_database(test_db)
+
+  sql_statement <- paste0("UPDATE ", tbl_name, " SET ", updates, " WHERE ", record_to_update)
+  DBI::dbExecute(conn, sql_statement)
+  updated_record <- DBI::dbGetQuery(
+    conn,
+    paste0("SELECT * FROM ", tbl_name, " WHERE ", record_to_update)
+    ) |>
+    tibble::as_tibble()
+
+  disconnect_from_database(conn, test_db = test_db, confirm = FALSE)
+
+  return(updated_record)
+}
+
+verify_facility_name <- function(facility_name, test_db = FALSE) {
+  facility <- get_facilities(test_db = test_db) |>
+    filter(name == facility_name)
+
+  if(nrow(facility) == 0) {
+    rlang::abort("Sorry, this facility doesn't exist in the database.")
+  }
+
+  return(TRUE)
+}
+
+verify_facility_update <- function(facility_name, field, value, test_db = FALSE) {
+  verify_facility_name(facility_name, test_db = test_db)
+
+  field |>
+    purrr::walk(verify_field) |>
+    purrr::walk(name_update_check)
+
+  facility_to_update <- get_facilities(test_db = test_db) |>
+    filter(name == facility_name)
+
+  record_count_check(facility_to_update)
+
+  return(facility_to_update)
+}
+
+verify_field <- function(field) {
   modifiable_columns <- get_facilities() |> colnames()
 
   if(!field %in% modifiable_columns) {
-    return("This field doesn't existing in the facilities table. Did you spell it correctly?")
+    rlang::abort(
+      "This field doesn't exist in the facilities table. Did you spell it correctly?"
+      )
   }
 
-  if(field != "name") {
-    facility_updated <- get_facilities(test_db = test_db) |>
-      filter(name == facility_name) |>
-      mutate(
-        {{field}} := {{value}},
-        modified = TRUE
-        )
-  } else {
-    return("You cannot update the facility's primary name this way. Please use insert_name() insted to create an alias.")
+  return(TRUE)
+}
+
+find_field <- function(to_match, field_list) {
+  matching_field <- match(to_match, field_list)
+
+  if(is.na(matching_field)) {
+    return(FALSE)
   }
 
-  if(nrow(facility_updated) == 1) {
-    if(field == "local_board") {
-      facility_tables <- get_facility_tables(test_db = test_db)
-
-      assets_check <- left_join(
-        facility_updated |> select("facility_id", "name"),
-        facility_tables[[1]],
-        by = c("facility_id", "name")
-      )
-
-      spaces_check <- left_join(
-        facility_updated |> select("facility_id", "name"),
-        facility_tables[[2]],
-        by = c("facility_id", "name")
-      )
-
-      entities_check <- left_join(
-        facility_updated |> select("facility_id", "name"),
-        facility_tables[[3]],
-        by = c("facility_id", "name")
-      )
-
-      return(assets_check)
-    } else {
-      facility_id <- facility_updated |> pull(facility_id)
-
-      conn <- connect_to_production_database(read_only = FALSE)
-
-      DBI::dbExecute(
-        conn,
-        paste0("UPDATE facilities_attributes SET ", field, "='", value, "' WHERE facility_id='", facility_id, "'")
-        )
-
-      disconnect_from_database(conn, confirm = FALSE)
-
-      updated_facility <- get_facilities(facility_id == {{facility_id}})
-
-      return(updated_facility)
-    }
-  } else {
-    return("You must update exactly one facility when calling this function.")
-  }
+  return(matching_field)
 }
--- tests/testthat/test-update.R
+++ tests/testthat/test-update.R
@@ -1,3 +1,88 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("we receive an error message when we pass a field called 'name'", {
+  expect_error(name_update_check(field = "name"))
+  expect_true(name_update_check(field = "field_name"))
+})
+
+test_that("the fields we want to update do exist in the facilities_attributes table", {
+  expect_error(verify_field(field = "made_up_field"))
+  expect_true(verify_field(field = "delivery_model"))
+})
+
+test_that("we're only able to update one record at a time", {
+  many_facilities <- get_facilities(test_db = TRUE)
+  one_facility <- many_facilities |> head(1)
+
+  expect_error(record_count_check(many_facilities))
+  expect_true(record_count_check(one_facility))
+})
+
+test_that("we can verify that the facility exists in the database", {
+  fake_facility <- "Narnia Library"
+  real_facility <- "Franklin Arts Centre"
+
+  expect_error(verify_facility_name(fake_facility, test_db = TRUE))
+  expect_true(verify_facility_name(real_facility, test_db = TRUE))
+})
+
+test_that("we can prepare a facility record for updating", {
+
+  facility_name = "Buckland Community Centre"
+  field = list("physical_address", "closed")
+  value = list("Corner Logan and Buckville Road", TRUE)
+
+  facility_to_update <- verify_facility_update(facility_name, field, value, test_db = TRUE)
+
+  expect_equal(facility_to_update |> pull(name), facility_name)
+
+})
+
+test_that("we can find a field from a list of field names", {
+  local_board_included <- list("facility_name", "local_board", "delivery_model")
+  local_board_not_included <- list("delivery_model", "closed")
+
+  expect_false(find_field("local_board", local_board_not_included))
+  expect_equal(find_field("local_board", local_board_included), 2)
+})
+
+test_that("we can update a field in the assets table", {
+  facility_name <- "Buckland Community Centre"
+  field <- "physical_address"
+  value <- "Corner Logan and Buckville Road"
+
+  updated_asset <- update_asset(facility_name, field, value, test_db = TRUE)
+
+  expect_equal(updated_asset |> pull(physical_address), "Corner Logan and Buckville Road")
+})
+
+test_that("we can successfully update the Local Board", {
+  updated_asset <- update_local_board("Buckland Community Centre", "Rodney", test_db = TRUE)
+
+  expect_equal(updated_asset |> pull(local_board), "Rodney")
+})
+
+test_that("we can successfully update the physical address", {
+  updated_asset <- update_address("Buckland Community Centre", "Corner Logan and Buckville Road", test_db = TRUE)
+
+  expect_equal(updated_asset |> pull(physical_address), "Corner Logan and Buckville Road")
+})
+
+test_that("we can update the facilities_attributes table", {
+  facility_name <- "Buckland Community Centre"
+  field <- list("closed", "designation")
+  value <- list(TRUE, "Community Centre")
+
+  updated_attributes <- update_facilities_attributes(facility_name, field, value, test_db = TRUE)
+
+  expect_equal(updated_attributes |> pull(designation), c("Rural Hall", "Community Centre"))
+  expect_equal(updated_attributes |> pull(closed), c(TRUE, FALSE))
+})
+
+test_that("we can update information about a facility", {
+  facility_name <- "Buckland Community Centre"
+  field <- list("physical_address", "designation")
+  value <- list("Corner Logan and Buckville Road", "Community Centre")
+
+  updated_facility <- update_facility(facility_name, field, value, test_db = TRUE)
+
+  expect_equal(updated_facility |> nrow(), 1)
 })
